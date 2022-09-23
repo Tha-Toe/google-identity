633c4e123f1a538951c7908635fdf4f62f32827f
tha toe saung <thatoe510@gmail.com>
2022-09-23 15:48:25 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useScript } from "./hook/useScript";
+import jwt_decode from "jwt-decode";
+import { useRef, useState } from "react";
 function App() {
+  const googleButtonRef = useRef(null);
+  const [user, setUser] = useState(null);
+
+  const onGoogleSignIn = (user) => {
+    let userCred = user.credential;
+    let payload = jwt_decode(userCred);
+    setUser(payload);
+  };
+
+  useScript("https://accounts.google.com/gsi/client", () => {
+    window.google.accounts.id.initialize({
+      client_id:
+        "555618407648-lkittruvsnt5jr327s088990pgv3bi9t.apps.googleusercontent.com",
+      callback: onGoogleSignIn,
+      auto_select: false,
+    });
+
+    window.google.accounts.id.renderButton(googleButtonRef.current, {
+      size: "medium",
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Identity Method</h1>
+      {!user && <div ref={googleButtonRef}></div>}{" "}
+      {user && (
+        <div>
+          <h1>{user.name}</h1>
+          <img src={user.picture} />
+          <p>{user.email}</p>
+          <button
+            style={{ marginTop: "50px", padding: "20px 40px" }}
+            onClick={() => {
+              setUser(null);
+            }}
+          >
+            LogOut
+          </button>
+        </div>
+      )}
     </div>
   );
 }
