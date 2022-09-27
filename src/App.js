11f3ea4b0a9423c61cd9b8ea1e716aeb6401cec7
tha toe saung <thatoe510@gmail.com>
2022-09-27 18:39:52 +0630

    gapi method

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import "./App.css";
-import { useScript } from "./hook/useScript";
-import jwt_decode from "jwt-decode";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import { GoogleLogin, GoogleLogout } from "@leecheuk/react-google-login";
+import { gapi } from "gapi-script";
+
+const clientId =
+  "555618407648-lkittruvsnt5jr327s088990pgv3bi9t.apps.googleusercontent.com";
 function App() {
   const googleButtonRef = useRef(null);
   const [user, setUser] = useState(null);
 
-  const onGoogleSignIn = (user) => {
-    let userCred = user.credential;
-    let payload = jwt_decode(userCred);
-    setUser(payload);
-  };
-
-  useScript("https://accounts.google.com/gsi/client", () => {
-    window.google.accounts.id.initialize({
-      client_id:
-        "555618407648-lkittruvsnt5jr327s088990pgv3bi9t.apps.googleusercontent.com",
-      callback: onGoogleSignIn,
-      auto_select: false,
-    });
-
-    window.google.accounts.id.renderButton(googleButtonRef.current, {
-      size: "medium",
-    });
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
   });
 
+  const onSuccess = (res) => {
+    console.log("success:", res.profileObj);
+    setUser(res.profileObj);
+  };
+  const onFailure = (err) => {
+    console.log("failed:", err);
+  };
+
+  const logOut = () => {
+    setUser(null);
+  };
   return (
     <div className="App">
       <h1>Google Identity Method</h1>
-      {!user && <div ref={googleButtonRef}></div>}{" "}
+      {!user && (
+        <div className="google-container">
+          <div style={{ opacity: "0", zIndex: "10" }}>
+            <GoogleLogin
+              clientId={clientId}
+              buttonText="Sign in with Google"
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy={"single_host_origin"}
+              isSignedIn={true}
+            />
+          </div>
+          <div className="google-button">Log in with google</div>
+        </div>
+      )}{" "}
       {user && (
         <div>
           <h1>{user.name}</h1>
-          <img src={user.picture} />
+          <img src={user.imageUrl} />
           <p>{user.email}</p>
-          <button
-            style={{ marginTop: "50px", padding: "20px 40px" }}
-            onClick={() => {
-              setUser(null);
-            }}
-          >
-            LogOut
-          </button>
+
+          <div className="google-container">
+            <div style={{ opacity: "0", zIndex: "10" }}>
+              <GoogleLogout
+                clientId={clientId}
+                buttonText="Log out"
+                onLogoutSuccess={logOut}
+              />
+            </div>
+            <div className="google-button">Log out</div>
+          </div>
         </div>
       )}
     </div>
